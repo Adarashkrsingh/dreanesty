e7a564c1d27eeea48e340d70b538b48a36879792
Adarashkrsingh <124179251+Adarashkrsingh@users.noreply.github.com>
2025-10-16 10:27:24 +0530

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -16,7 +16,7 @@ const LoginPage = () => {
     e.preventDefault()
 
     try {
-      const response = await fetch ("http://localhost:3002/auth/login", {
+      const response = await fetch ("https://dreamnestserver-1.onrender.com/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
