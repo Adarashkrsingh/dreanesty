50101252b8776850b5438095c09a29c617ae6bb3
Adarashkrsingh <124179251+Adarashkrsingh@users.noreply.github.com>
2025-12-16 00:18:24 +0530

    Update TripList.jsx

--- src/pages/TripList.jsx
+++ src/pages/TripList.jsx
@@ -17,7 +17,7 @@ const TripList = () => {
   const getTripList = async () => {
     try {
       const response = await fetch(
-        `https://dreamnestserver-1.onrender.com/users/${userId}/trips`,
+        `https://dreamnestserver-2.onrender.com/users/${userId}/trips`,
         {
           method: "GET",
         }
